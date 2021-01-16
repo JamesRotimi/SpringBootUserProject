edd2adbd7e6b1ce2c59c40faea435cfc721cee6a
jamesrotimi <jamesrotimi@icloud.com>
2021-01-16 16:30:02 +0000

    added more unit test shouldselectUserById & Not & SelectAllUsers

--- src/test/java/com/rotimi/dao/FakeDataDaoTest.java
+++ src/test/java/com/rotimi/dao/FakeDataDaoTest.java
@@ -4,9 +4,14 @@ package com.rotimi.dao;
 import com.rotimi.model.UserProfile;
 import com.rotimi.model.UserProfile.Gender;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class FakeDataDaoTest {
 
@@ -31,7 +36,23 @@ class FakeDataDaoTest {
   }
 
   @Test
-  void selectUserByUserUid() {
+  void shouldSelectUserByUserUid() {
+    UUID AnnaUid = UUID.randomUUID();
+    UserProfile userProfile = new UserProfile(AnnaUid,"anna",
+        "hannah", 12,"anna@gmail.com",Gender.FEMALE);
+        fakeDataDao.insertUser(AnnaUid,userProfile);
+        assertThat(fakeDataDao.selectAllUsers().size(),equalTo(2));
+    Optional<UserProfile> AnnaOptional = fakeDataDao.selectUserByUserUid(AnnaUid);
+
+    assertThat(fakeDataDao.selectUserByUserUid(AnnaUid).isPresent(),equalTo(true));
+    assertThat(fakeDataDao.selectUserByUserUid(AnnaUid),equalTo(AnnaOptional));
+    assertThat(AnnaOptional.get(),equalTo(userProfile));
+  }
+
+  @Test
+  void shouldNotSelectUserByRandomUserUid() {
+    Optional<UserProfile> userProfile = fakeDataDao.selectUserByUserUid(UUID.randomUUID());
+    assertThat(userProfile.isPresent(),equalTo(false));
   }
 
   @Test
