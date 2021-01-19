489806247ea769d4740e864d4d4fd36485ef0442
jamesrotimi <jamesrotimi@icloud.com>
2021-01-19 15:26:08 +0000

    adding Post and Delete Endpoint

--- src/main/java/com/rotimi/learningspringboot/controller/UserController.java
+++ src/main/java/com/rotimi/learningspringboot/controller/UserController.java
@@ -6,13 +6,18 @@ import com.rotimi.learningspringboot.service.UserProfileService;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,7 +44,52 @@ public class UserController {
     if (UserProfileOpt.isPresent()) {
       return  ResponseEntity.ok(UserProfileOpt.get());
     }
-   return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("user" + userUId + "was not found");
+   return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("user not found"));
+  }
+
+  @RequestMapping
+      (method = RequestMethod.POST,
+          consumes = MediaType.APPLICATION_JSON)
+  public ResponseEntity<Integer> createUserProfile(@RequestBody UserProfile createUserProfile){
+    int updateUser1 = userProfileService.insertUser(createUserProfile);
+    return getIntegerResponseEntity(updateUser1);
+  }
+
+  @RequestMapping
+      (method = RequestMethod.PUT,
+          consumes = MediaType.APPLICATION_JSON)
+  public ResponseEntity<Integer> updateUser(@RequestBody UserProfile updateUser){
+    int updateUser1 = userProfileService.updateUser(updateUser);
+    return getIntegerResponseEntity(updateUser1);
+  }
+
+  @RequestMapping
+      (method = RequestMethod.DELETE,
+       path = "{userUId}")
+  public ResponseEntity<Integer> deleteUserProfile(@PathVariable UUID userUId){
+    int deleteUser1 = userProfileService.deleteUserByUserUid(userUId);
+    return getIntegerResponseEntity(deleteUser1);
+  }
+
+
+  private ResponseEntity<Integer> getIntegerResponseEntity(int updateUser1) {
+    if (updateUser1 == 1) {
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.badRequest().build();
+  }
+
+  class ErrorMessage {
+
+    private String message;
+
+    public ErrorMessage(String message) {
+      this.message = message;
+    }
+
+    public String getMessage() {
+      return message;
+    }
   }
 
 //  @GetMapping
